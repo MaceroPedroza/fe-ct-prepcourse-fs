24b5ff7a8becf27cdbf3b96498c927571ec752ef
mordico <moisesmacero56@gmai.com>
2024-01-24 03:39:40 -0400

    Mitad de la tarea M6

--- M05 Fundamentos Javascript/ejercicios/14.js
+++ M05 Fundamentos Javascript/ejercicios/14.js
@@ -3,6 +3,12 @@ function tienenMismaLongitud(str1, str2) {
   // Retorna true si los dos strings tienen la misma longitud.
   // De lo contrario, retorna false.
   // Tu código:
+  var longitud = str1.length
+  var longitud2= str2.length
+  if (longitud == longitud2){ 
+    return true
+  }
+  else return false 
 }
 
 module.exports = tienenMismaLongitud;
--- M05 Fundamentos Javascript/ejercicios/19.js
+++ M05 Fundamentos Javascript/ejercicios/19.js
@@ -5,6 +5,14 @@ function esPositivo(num) {
   // Si el número es negativo ---> "Es negativo".
   // Si el número es 0, devuelve false.
   // Tu código:
+  var indicador = Math.sign(num)
+  if (indicador == 1)
+  return "Es positivo"
+  else if (indicador == -1)
+  return "Es negativo"
+  else (indicador = 0)
+  return false
+
 }
 
 module.exports = esPositivo;
